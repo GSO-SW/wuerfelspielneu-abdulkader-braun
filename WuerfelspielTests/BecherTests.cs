003e110a136c5c97f29bf530d4731b79dd9e1402
Justus-Braun <Justus.b4@P-0G-0B009-0371>
2022-04-01 12:22:55 +0200

    Becher Erstellen Test Hinzugefügt

--- WuerfelspielTests/BecherTests.cs
+++ WuerfelspielTests/BecherTests.cs
@@ -7,6 +7,17 @@ namespace WuerfelspielTests
     [TestClass]
     class BecherTests
     {
-        
+        [TestMethod]
+        private void Becher_Erstellen()
+        {
+            // Arrange
+            int anzahlWuerfel = 5;
+
+            //Act
+            Becher b = new Becher(anzahlWuerfel);
+
+            //Assert
+            Assert.IsInstanceOfType(b, Becher);
+        }
     }
 }
